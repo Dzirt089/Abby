a75bb9d06349f5eab126c1238a2fd3d5dd9e4b0c
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-10-16 16:46:27 +0300

    Добавьте файлы проекта.

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace AbbyWeb.Model
+{
+    public class Category
+    {
+
+        /// <summary>
+        /// Индентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование категории
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Порядок отображения столбца таблицы
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+}
